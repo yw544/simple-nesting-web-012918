446923d5f66af3527086bf726d27f3bdcb8794fe
York Wang <yw544@nyu.edu>
2018-01-11 08:09:32 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -109,7 +109,8 @@ def changing_alan
         }
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable.
-		 alans_new_info = programmer_hash[:alan_kay]
+		alan = programmer_hash[:alan_kay]
+		alans_new_info = alan[:known_for]
 
 		 alans_new_info = "GUI"
 
